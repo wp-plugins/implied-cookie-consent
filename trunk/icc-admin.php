84444b96737db99286d24e24125b2754da321732
Senktec <Senktec@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-09-04 13:52:18 +0000

    Update 1.1
    git-svn-id: https://plugins.svn.wordpress.org/implied-cookie-consent/tags/1.1@979376 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ trunk/icc-admin.php
@@ -0,0 +1,108 @@
+<?php
+
+function icc_activation(){
+	$cookie_message = __( 'This site uses cookies. <a href="/cookies">Find out more about this site&rsquo;s cookies.</a> [icc_dismiss]', 'implied-cookie-consent' );
+
+	$cookie_page_name =  __( 'Cookies', 'implied-cookie-consent' );
+	$cookie_page_content = __( '<h1>Cookies</h1>
+<p>This site uses cookies. Most websites do this. Our cookies aren\'t used to identify you personally, they\'re here to make the site work better and allow us to analyse how it\'s being used. You can delete these cookies as you wish, but some aspects of the site may not work correctly without cookies.</p>
+<p>To learn more about cookies and how to manage them, visit <a href="http://aboutcookies.org">AboutCookies.org</a>.</p>
+<h3>Specific Cookies Used</h3>
+<ul>
+	<li>Google Analiyics Cookies &mdash; These cookies are used to track how people use the site. These cookies include _utma, _utmb, _utmc, and _utmz.</li>
+	<li>Account Cookies &mdash; If you have an account or comment on this site, cookies are used verify you are the same person when you are logged in, and your preferences. These include cookies beginning with wordpress_ and wp-settings-.</li>
+	<li>Cookie Message &mdash; Once you have been informed about cookies, a cookie is stored to track that you have seen the message, and it won\'t be shown again. This cookie is named icc_cookie_message.
+</ul>
+<p><strong>This page should be completed by the website admin...</strong></p>', 'implied-cookie-consent' );
+
+	// Setup default options
+	$opt_name = 'implied_cookie_consent';
+	$options = get_option( $opt_name );
+	if ( !is_array( $options ) ) {
+		$options = array(
+			'message' => $cookie_message,
+			'bgcolor' => '#dddddd',
+		);
+		update_option( $opt_name, $options );
+	}
+
+	// Create cookies page
+	$cpage = get_page_by_title ( $cookie_page_name );
+	if ( !$cpage ) {
+		global $user_ID;
+		$page = array();
+		$page['post_type']    = 'page';
+		$page['post_title']   = $cookie_page_name;
+		$page['post_content'] = $cookie_page_content;
+		$page['post_parent']  = 0;
+		$page['post_author']  = $user_ID;
+		$page['post_status']  = 'publish';
+		$pageid = wp_insert_post ( $page );
+	}
+
+}
+
+
+add_action( 'admin_enqueue_scripts', function() {
+	wp_enqueue_style( 'wp-color-picker' );
+	wp_enqueue_script('wp-color-picker');
+
+	wp_register_script('implied_cookie_consent_admin_js', plugins_url('icc-admin.js', __FILE__ ));
+	wp_enqueue_script('implied_cookie_consent_admin_js');
+});
+
+
+add_filter( 'plugin_action_links', function($links, $file){
+	if ( $file == 'implied-cookie-consent/implied-cookie-consent.php' ) {
+		array_unshift( $links, '<a href="' . admin_url('options-general.php?page=implied-cookie-consent.php') . '">' . __( 'Settings', 'implied-cookie-consent' ) . '</a>' );
+	}
+	return $links;
+}, 10, 2 );
+  
+
+add_action('admin_menu', function(){
+	add_options_page( 
+		__( 'Implied Cookie Consent Options', 'implied-cookie-consent' ),
+		__( 'Implied Cookie Consent', 'implied-cookie-consent' ),
+		'manage_options', 'implied-cookie-consent.php', function(){
+		
+		if ( !current_user_can( 'manage_options' ) )  {
+			wp_die( __( 'You do not have permission to access this page.', 'implied-cookie-consent' ) );
+		}
+
+		$opt_name = 'implied_cookie_consent';
+		$opt_message_name = 'icc_message';
+		$opt_bgcolor_name = 'icc_bgcolor';
+		$hidden_field_name = 'icc_form_hidden';
+
+		$options = get_option( $opt_name );
+
+		if( isset($_POST[ $hidden_field_name ]) && $_POST[ $hidden_field_name ] == 'Y' ) {
+			$options['message'] = wp_kses_stripslashes( $_POST[ $opt_message_name ] );
+			$options['bgcolor'] = $_POST[ $opt_bgcolor_name ];
+			update_option( $opt_name, $options );
+			echo '<div class="updated"><p><strong>' . __( 'Options saved.', 'implied-cookie-consent' ) . '</strong></p></div>';
+		}
+?>
+		<div class="wrap">
+			<?php screen_icon(); ?>
+			<h2><?php _e( 'Implied Cookie Consent Options', 'implied-cookie-consent' ); ?></h2>
+
+			<form name="icc_form" method="post" action="<?php echo str_replace( '%7E', '~', $_SERVER['REQUEST_URI']); ?>">  
+				<input type="hidden" name="<?php echo $hidden_field_name; ?>" value="Y"> 
+
+				<p>
+					<label for="<?php echo $opt_message_name; ?>"><?php _e("Cookie info message displayed to the user (optionally use the [icc_dismiss] shortcode to add a dismiss button): ", 'implied-cookie-consent'); ?></label><br>
+					<textarea name="<?php echo $opt_message_name; ?>" cols="80" rows="3"><?php echo $options['message']; ?></textarea>
+				</p>
+				<p>
+					<label for="<?php echo $opt_bgcolor_name; ?>"><?php _e("Cookie info message background color: ", 'implied-cookie-consent'); ?></label><br>
+					<input type="text" name="<?php echo $opt_bgcolor_name; ?>" value="<?php echo $options['bgcolor']; ?>" size="20" class="icc_color_field">
+				</p>
+
+				<?php submit_button(); ?>
+			</form>  
+		</div>  
+<?php
+	});
+});
---
+++ trunk/readme.txt
@@ -0,0 +1,83 @@
+=== Implied Cookie Consent ===
+Contributors: Senktec
+Tags: cookie law, cookie warning, cookie consent
+Tested up to: 4.0.0
+Requires at least: 3.0.1
+Stable tag: 1.1
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+
+== Description ==
+
+This plugin displays a customisable unobtrusive information bar at the top of the page informing the user about the site's use of cookies. This approach follows an implied consent model. It doesn't explicitly ask the user for consent before placing cookies, it merely informs them about the use of cookies. When the user navigates to a second page or clicks on the dismiss button, the bar is hidden. This approach is similar to that used on many government websites including gov.uk. 
+
+The plugin has settings in the wordpress admin which allow the colour and content of the information bar to be customised.
+
+An example cookie information page will be created on installation of the plugin. It is up to you to correctly populate the text on that page.
+
+= Why another cookie plugin? =
+
+There are many WordPress cookie plugins available but none of them seemed to do what I wanted. They typically tend to provide a very intrusive experience for the user (e.g. via modal popups), or are too complex to configure.
+
+= Technical Details =
+
+This plugin uses jQuery (JavaScript) to show and hide the information bar. A cookie is stored via the jQuery Cookie Plugin (included with this plugin) to track that the message has been displayed and does not need to be shown on each subsequent page view. 
+
+
+== Installation ==
+
+1. Install this plugin in the usual way in 'wp-content/plugins'.
+2. Customise the info bar message and colour via the settings page.
+3. Customise the cookie information page.
+
+
+== Frequently Asked Questions ==
+
+= Can I do additional styling of the cookie info bar in CSS? =
+
+Yes, you can get a handle via the 'icc_message' CSS ID:
+
+`#icc_message {
+    color: green;	
+}`
+
+
+= Can I use my own page to display further information about Cookies? =
+
+Yes. This plugin creates a default page for you, but you don't have to use it, and you can delete it. You can create your own page wherever you like and update the cookie info bar message within the admin settings to link to the correct page.
+
+= Does this plugin analyse the cookies my site uses? =
+
+No. It is up to you to determine which cookies are used on your site and ensure the content on the page is appropriate.
+
+= Does installing this plugin guarantee compliance with the EU Cookie Law? =
+
+The plugin author can't provide any guarantee that installing this plugin will make your site compliant with the EU Cookie Law. You should verify that the consent model used here is appropriate for your site.
+
+
+== Screenshots ==
+
+1. The information bar in the page header.
+2. Admin settings.
+
+
+== Upgrade Notice ==
+
+= 1.1 =
+Improve support for multi-language.
+
+= 1.0 =
+First version of this plugin.
+
+
+== Changelog ==
+
+= 1.1 =
+* Improve support for i18n.
+* Add POT file.
+
+= 1.0 =
+* First version of this plugin.
+* Updates to readme.txt.
+
